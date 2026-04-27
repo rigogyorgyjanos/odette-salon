c413e103852c344904f66906e4ec3a48f8bb42f3
rigogyorgyjanos <rigo.gyorgyj@gmail.com>
2026-04-27 16:46:46 +0200

    add 3d insight view

---
+++ app/inside/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Inside() {
+  return (
+    <iframe
+      src="/inside/index.htm"
+      style={{ width: "100%", height: "100vh", border: "none" }}
+    />
+  );
+}
--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+ 
   output: "export",
-
-
   images: {
     unoptimized: true,
   },
+
 }
 
 export default nextConfig;
